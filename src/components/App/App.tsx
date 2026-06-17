40b8098f1a8d48ce203eee590382f2f0ae017639
Maria Khomynets <masha.khomynets@gmail.com>
2026-06-17 10:33:39 +0300

    add modal

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,11 +1,15 @@
+import MovieModal from "../MovieModal/MovieModal";
 import { useState } from "react";
-
-import css from "./App.module.css";
-
 export default function App() {
-  const [count, setCount] = useState(0);
-
-  return ();
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const openModal = () => setIsModalOpen(true);
+  const closeModal = () => setIsModalOpen(false);
+  return (
+    <>
+      <button onClick={openModal}>Open modal</button>
+      {isModalOpen && (
+        <MovieModal movie={"<h2>Modal Title</h2>"} onClose={closeModal} />
+      )}
+    </>
+  );
 }
-
-
